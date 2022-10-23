84c2f8c9f3fb8a6cac8620a7c1b6c3585cb26f73
Ralph-1006 <ralphboehm2002@yahoo.de>
2022-10-23 18:53:58 +0200

    add timer class and test

---
+++ 2-time-analysis-application/src/test/java/useCases/TimerTest.java
@@ -0,0 +1,14 @@
+package useCases;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class TimerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
