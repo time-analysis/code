ba1cf1f43dc252a5b76e9c0fd0c3ab3920788af9
Franziska1402 <85218663+Franziska1402@users.noreply.github.com>
2022-10-25 10:03:39 +0200

    changed UTF format for test file

--- 2-time-analysis-application/src/test/java/useCases/TimerTest.java
+++ 2-time-analysis-application/src/test/java/useCases/TimerTest.java
@@ -13,27 +13,28 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TimerTest {
-	@BeforeEach
-	void setup() {
-	 Timer.setEntry(new Entry(EntryType.PRESENCE,"hehe"));
-	 Timer timer = new Timer();
-	}
+    @BeforeEach
+    void setup() {
+        Timer.setEntry(new Entry(EntryType.PRESENCE, "hehe"));
+        Timer timer = new Timer();
+    }
 
-	@Test
-	void timerStart() {
-		Timer.setRunning(false);
-		Timer.toggleTimer();
-		LocalDateTime currentTime = LocalDateTime.now();
-		assertTrue(Timer.isRunning());
-		assertTrue(Duration.between(Timer.getEntry().getStart(),currentTime).toMillis()<100); //10 ms can pass between starting timer and measuring time...
-	}
-	@Test
-	void timerEnd() {
-		Timer.setRunning(true);
-		Timer.toggleTimer();
-		LocalDateTime currentTime = LocalDateTime.now();
-		assertFalse(Timer.isRunning());
-		assertTrue(Duration.between(Timer.getEntry().getEnd(),currentTime).toMillis()<100); //10 ms can pass between starting timer and measuring time...
-	}
+    @Test
+    void timerStart() {
+        Timer.setRunning(false);
+        Timer.toggleTimer();
+        LocalDateTime currentTime = LocalDateTime.now();
+        assertTrue(Timer.isRunning());
+        assertTrue(Duration.between(Timer.getEntry().getStart(), currentTime).toMillis() < 100); //10 ms can pass between starting timer and measuring time...
+    }
+
+    @Test
+    void timerEnd() {
+        Timer.setRunning(true);
+        Timer.toggleTimer();
+        LocalDateTime currentTime = LocalDateTime.now();
+        assertFalse(Timer.isRunning());
+        assertTrue(Duration.between(Timer.getEntry().getEnd(), currentTime).toMillis() < 100); //10 ms can pass between starting timer and measuring time...
+    }
 
 }
