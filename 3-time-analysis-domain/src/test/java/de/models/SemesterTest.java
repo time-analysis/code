0bfc19ed5577b5248c78d9fb9182bab862b65ede
Franziska1402 <85218663+Franziska1402@users.noreply.github.com>
2023-01-31 11:52:59 +0100

    added tests for exceptions, code coverage 100%

--- 3-time-analysis-domain/src/test/java/de/models/SemesterTest.java
+++ 3-time-analysis-domain/src/test/java/de/models/SemesterTest.java
@@ -19,4 +19,22 @@ class SemesterTest {
         assertEquals(semester.getEnd(), now);
     }
 
+    @Test
+    void throwsErrorIfStartIsAfterEnd() {
+        String semesterWithStartAfterEnd = "SemesterWithStartAfterEnd";
+        LocalDate start = LocalDate.of(2023, 01, 31);
+        LocalDate end = LocalDate.of(2022, 01, 29);
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> new Semester(semesterWithStartAfterEnd, start, end));
+        assertEquals("start cannot be after end", exception.getMessage());
+    }
+
+    @Test
+    void throwsErrorIfNoNameIsGiven() {
+        LocalDate end = LocalDate.of(2023, 12, 07);
+        LocalDate start = LocalDate.of(2023, 01, 13);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Semester("", start, end));
+        assertEquals("name must contain at least one letter", exception.getMessage());
+    }
 }
