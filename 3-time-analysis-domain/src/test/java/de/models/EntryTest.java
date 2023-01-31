e0cf39dd32816d5dcf948113e81b338ca3ad92a4
Ralph-1006 <ralphboehm2002@yahoo.de>
2023-01-31 10:54:17 +0100

    udpate entryTest

--- 3-time-analysis-domain/src/test/java/de/models/EntryTest.java
+++ 3-time-analysis-domain/src/test/java/de/models/EntryTest.java
@@ -5,51 +5,51 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EntryTest {
+    
+    @Test
+    void startHasToBeBeforeEnd() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime start = LocalDateTime.parse("2022-10-30 10:00", formatter);
+        LocalDateTime end = LocalDateTime.parse("2022-10-30 09:00", formatter);
+        Semester semester = new Semester("5. Semester", LocalDate.now(), LocalDate.now());
+        Lecture lecture = new Lecture("ASE", semester, 10, 20);
+        assertThrows(IllegalStateException.class,
+                () -> new Entry(start, EntryType.PRESENCE, "test", lecture).setEnd(end));
 
-    //TODO new tests needed because of the refactoring of Entry
-//    @Test
-//    void startHasToBeBeforeEnd() {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-//        LocalDateTime start = LocalDateTime.parse("2022-10-30 10:00", formatter);
-//        LocalDateTime end = LocalDateTime.parse("2022-10-30 09:00", formatter);
-//        Semester semester = new Semester("5. Semester", LocalDate.now(), LocalDate.now());
-//        Lecture lecture = new Lecture("ASE", semester, 10, 20);
-//        assertThrows(IllegalStateException.class,
-//                () -> new Entry(start, end, EntryType.PRESENCE, "test", lecture));
-//
-//    }
-//
-//    @Test
-//    void testConstructor() {
-//        LocalDateTime startEnd = LocalDateTime.now();
-//        LocalDate now = LocalDate.now();
-//        Semester semster = new Semester("5. Semester", now, now);
-//        Lecture lecture = new Lecture("ASE", semster, 50, 10);
-//        Entry entry = new Entry(startEnd, startEnd, EntryType.STUDY, "more information", lecture);
-//
-//        assertEquals(entry.getStart(), startEnd);
-//        assertEquals(entry.getEnd(), startEnd);
-//        assertEquals(entry.getType(), EntryType.STUDY);
-//        assertEquals(entry.getDetails(), "more information");
-//        assertEquals(entry.getLecture(), lecture);
-//    }
-//
-//    @Test
-//    void testConstructorWithoutStartAndEnd() {
-//        LocalDate now = LocalDate.now();
-//        Semester semster = new Semester("5. Semester", now, now);
-//        Lecture lecture = new Lecture("ASE", semster, 50, 10);
-//        Entry entry = new Entry(EntryType.STUDY, "more information", lecture);
-//
-//        assertTrue(Duration.between(entry.getStart(), LocalDateTime.now()).toMillis() < 10);
-//        assertTrue(Duration.between(entry.getEnd(), LocalDateTime.now()).toMillis() < 10);
-//        assertEquals(entry.getType(), EntryType.STUDY);
-//        assertEquals(entry.getDetails(), "more information");
-//        assertEquals(entry.getLecture(), lecture);
-//    }
+    }
+
+    @Test
+    void testConstructor() {
+        LocalDateTime startEnd = LocalDateTime.now();
+        LocalDate now = LocalDate.now();
+        Semester semester = new Semester("5. Semester", now, now);
+        Lecture lecture = new Lecture("ASE", semester, 50, 10);
+        Entry entry = new Entry(startEnd, EntryType.STUDY, "more information", lecture);
+        entry.setEnd(startEnd);
+
+        assertEquals(entry.getStart(), startEnd);
+        assertEquals(entry.getEnd(), startEnd);
+        assertEquals(entry.getType(), EntryType.STUDY);
+        assertEquals(entry.getDetails(), "more information");
+        assertEquals(entry.getLecture(), lecture);
+    }
+
+    @Test
+    void testCalculateDuration() {
+        LocalDate now = LocalDate.now();
+        LocalDateTime start = LocalDateTime.of(LocalDate.of(2022, 10, 10), LocalTime.of(10, 0, 0, 0));
+        LocalDateTime end = LocalDateTime.of(LocalDate.of(2022, 10, 10), LocalTime.of(11, 0, 0, 0));
+        Semester semester = new Semester("5. Semester", now, now);
+        Lecture lecture = new Lecture("ASE", semester, 50, 10);
+        Entry entry = new Entry(start, EntryType.STUDY, "more information", lecture);
+        entry.setEnd(end);
+        assertEquals(entry.calculateDuration(), Duration.ofHours(1));
+
+    }
 }
