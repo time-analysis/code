6ece7db3594419c39237c2abbb3aae47054360e9
Ralph-1006 <ralphboehm2002@yahoo.de>
2023-03-01 21:22:04 +0100

    implement getLecture and getLectureByName

--- 0-time-analysis-plugin-database/src/main/java/useCases/DataPlugin.java
+++ 0-time-analysis-plugin-database/src/main/java/useCases/DataPlugin.java
@@ -3,17 +3,24 @@ package useCases;
 import Interfaces.DataPluginInterface;
 import ressourceModels.EntryRessource;
 import ressourceModels.LectureResource;
+import ressourceModels.SemesterRessource;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DataPlugin implements DataPluginInterface {
     private String entryFileName;
     private String lectureFileName;
+    private String semesterFileName;
 
-    public DataPlugin(String entryFileName, String lectureFileName) {
+    public DataPlugin(String entryFileName, String lectureFileName, String semesterFileName) {
         this.entryFileName = entryFileName;
         this.lectureFileName = lectureFileName;
+        this.semesterFileName = semesterFileName;
     }
 
     @Override
@@ -21,11 +28,12 @@ public class DataPlugin implements DataPluginInterface {
 
         String csvEntry = String.format(
                 "%s,%s,%s,%s,%s"
+                , entryRessource.getLecture()
                 , entryRessource.getStart()
                 , entryRessource.getEnd()
                 , entryRessource.getType()
                 , entryRessource.getDetails()
-                , entryRessource.getLecture());
+        );
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(entryFileName, true))) {
             writer.write(csvEntry);
             writer.newLine();
@@ -41,9 +49,9 @@ public class DataPlugin implements DataPluginInterface {
         String csvLecture = String.format(
                 "%s,%s,%s,%s"
                 , lectureResource.getName()
+                , lectureResource.getSemester()
                 , lectureResource.getLectureTime()
-                , lectureResource.getSelfStudyTime()
-                , lectureResource.getSemester());
+                , lectureResource.getSelfStudyTime());
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(lectureFileName, true))) {
             writer.write(csvLecture);
             writer.newLine();
@@ -54,7 +62,67 @@ public class DataPlugin implements DataPluginInterface {
     }
 
     @Override
-    public List<LectureResource> getLectures() {
+    public void persistSemester(SemesterRessource semester) {
+        String csvSemester = String.format(
+                "%s,%s,%s"
+                , semester.getName()
+                , semester.getStart()
+                , semester.getEnd());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(semesterFileName, true))) {
+            writer.write(csvSemester);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public Optional<SemesterRessource> getSemesterByName(String semesterName) {
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<LectureResource> getLectureByName(String lectureName) {
+        LectureResource lectureResource = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(lectureFileName))) {
+            List<String> lines = bufferedReader.lines().collect(Collectors.toList());
+            for (String line : lines) {
+                String[] split = line.split(",");
+                String name = split[0];
+                if (name.equals(lectureName)) {
+                    String semester = split[1];
+                    int lectureTime = Integer.parseInt(split[2]);
+                    int selfStudyTime = Integer.parseInt(split[3]);
+                    lectureResource = new LectureResource(name, semester, lectureTime, selfStudyTime);
+                }
+            }
+            if (Objects.isNull(lectureResource)) {
+                return Optional.empty();
+            } else {
+                return Optional.of(lectureResource);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<LectureResource> getLectures() {
+        List<LectureResource> lectureList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(lectureFileName))) {
+            bufferedReader.lines().forEach(line -> {
+                String[] split = line.split(",");
+                String name = split[0];
+                String semester = split[1];
+                int lectureTime = Integer.parseInt(split[2]);
+                int selfStudyTime = Integer.parseInt(split[3]);
+                lectureList.add(new LectureResource(name, semester, lectureTime, selfStudyTime));
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return lectureList;
     }
 }
