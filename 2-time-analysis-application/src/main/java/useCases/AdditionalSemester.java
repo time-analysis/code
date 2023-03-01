3f9f715276c8a22dfcc0e6ee4a30290b18fa99ab
Ralph-1006 <ralphboehm2002@yahoo.de>
2023-03-01 20:38:11 +0100

    create additional use cases

---
+++ 2-time-analysis-application/src/main/java/useCases/AdditionalSemester.java
@@ -0,0 +1,32 @@
+package useCases;
+
+import Interfaces.DataAdapterInterface;
+import Interfaces.DataPluginInterface;
+import Interfaces.UIPluginInterface;
+import de.models.Semester;
+import ressourceModels.LectureResource;
+import ressourceModels.SemesterRessource;
+
+import java.util.Objects;
+
+public class AdditionalSemester {
+
+    private DataAdapterInterface dataAdapter;
+    private DataPluginInterface dataPlugin;
+    private UIPluginInterface uiPlugin;
+
+    public void AdditionalSemester(DataAdapterInterface dataAdapter, DataPluginInterface dataPlugin, UIPluginInterface uiPlugin) {
+        this.dataAdapter = dataAdapter;
+        this.dataPlugin = dataPlugin;
+        this.uiPlugin = uiPlugin;
+    }
+
+    public void addSemester(Semester semester) {
+        if (dataPlugin.getSemesterByName(semester.getName()).isPresent()) {
+            uiPlugin.displayError("A Semester with the name  " + semester.getName() + "already exists.");
+        } else {
+            dataPlugin.persistSemester(dataAdapter.mapSemesterToSemesterRessource(semester));
+            uiPlugin.displayMessage("Semester created.");
+        }
+    }
+}
