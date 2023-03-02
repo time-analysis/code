aa9bdcd8a889a7f9a0a6a7875e69154f61d2339b
Ralph-1006 <ralphboehm2002@yahoo.de>
2023-03-02 20:57:10 +0100

    add todo

--- 0-time-analysis-plugin-database/src/main/java/useCases/DataPlugin.java
+++ 0-time-analysis-plugin-database/src/main/java/useCases/DataPlugin.java
@@ -23,7 +23,7 @@ public class DataPlugin implements DataPluginInterface {
         this.semesterFileName = semesterFileName;
     }
 
-    //todo check if files exists
+    //todo check if files exists, dont allow comma as input
     @Override
     public boolean persistEntry(EntryRessource entryRessource) {
 
